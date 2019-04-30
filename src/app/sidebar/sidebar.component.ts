135a74704e8654e806489d1ba35ef3ece2973592
Vohsty <stevekimanthi87@gmail.com>
2019-04-30 09:15:25 +0300

    add sidebar.component.ts content

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,19 +1,19 @@
-// import { Component, OnInit } from '@angular/core';
-// import { Router, NavigationEnd } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
-// @Component({
-//   selector: 'app-sidebar',
-//   templateUrl: './sidebar.component.html',
-//   styleUrls: ['./sidebar.component.css']
-// })
-// export class SidebarComponent implements OnInit {
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
 
-//   currentUrl: string;
+  currentUrl: string;
 
-//   constructor(private router: Router) { 
-//     router.events.subscribe((_: NavigationEnd) => this.currentUrl = _.url);
-//   }
+  constructor(private router: Router) { 
+    router.events.subscribe((_: NavigationEnd) => this.currentUrl = _.url);
+  }
 
-//   ngOnInit() {}
+  ngOnInit() {}
 
-// }
+}
